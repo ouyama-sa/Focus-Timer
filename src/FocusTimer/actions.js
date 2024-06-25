98d62e13ae968a488e610cedc2bac2db181c3bcd
ouyama-sa <ouyamasa@gmail.com>
2024-06-25 18:24:55 -0300

    modified:   src/FocusTimer/actions.js 	modified:   src/FocusTimer/elements.js 	modified:   src/FocusTimer/events.js 	modified:   src/FocusTimer/index.js 	new file:   src/FocusTimer/timer.js 	modified:   src/main.js

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,7 +1,25 @@
-export function toggleRunning() {
+import state from './state.js';
+import * as timer from './timer.js';
+import * as el from './elements.js'
 
+export function toggleRunning() {
+	state.isRunning = document.documentElement.classList.toggle('running');
+
+	timer.countDown();
+}
+
+export function reset() {
+	state.isRunning = false;
+	// different: toggle -> remove ?
+	document.documentElement.classList.toggle('running');
+	timer.updateDisplay();
 }
 
 export function set() {
+	el.minutes.setAttribute('contenteditable', true)
+	el.minutes.focus()
+}
 
+export function toggleMusic() {
+	state.isMute = document.documentElement.classList.toggle('music-on');
 }
